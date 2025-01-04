f66357f93215b5b21d17675750fabf770adacd5a
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2025-01-04 13:07:54 +0530

    Created the function to add custom command line option and a SetUp() pytest fixture.

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+
+# Using the pytest_addoption() function to introduce custom commandline parameters while running tests
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome", help="Mention the browser names from the following to run your tests: Chrome, Firefox")
+
+# Setup fixture with scope mentioned given as Class, hence it will only run once for each Class
+@pytest.fixture(scope='class')
+def SetUp(url,request):
+    browserName = request.config.getoption("-browser")
+    if browserName.lower() == 'chrome':
+        driver = webdriver.Chrome()
+    else:
+        driver = webdriver.Firefox()
+    driver.get(url)
+    driver.maximize_window()
+    request.cls.driver = driver
+    yield 
+    driver.quit()
