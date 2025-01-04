994863f85c24ab382298434a4e38cec08798ecc4
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2025-01-04 18:39:47 +0530

    Finished all the test cases for the Homepage.

--- Tests/test_Tools.py
+++ Tests/test_Tools.py
@@ -5,7 +5,6 @@ from PageObjects.HomePage import HomePage
 
 class TestingTools(BaseClass):
 
-    @pytest.mark.skip
     def test_TotalNumberOfTools(self):
         log = self.getLogger()
         homeObj = HomePage(self.driver)
@@ -23,11 +22,18 @@ class TestingTools(BaseClass):
             toolNameSplit = (tool.text).split(' ')
             for individualName in toolNameSplit:
                 if individualName[0].isnumeric() or individualName == 'with' or individualName[0] == '(':
-                    log.info("Skipped words {}".format(individualName))
+                    log.debug("Skipped words {}".format(individualName))
                     continue
                 assert individualName[0].isupper(), 'Issue with Grammar in tool naming.'
 
-
+    def test_PriceFormat(self):
+        log = self.getLogger()
+        homeObj = HomePage(self.driver)
+        self.verifyPageLoad(homeObj.getToolContainerElement()[1])
+        prices = homeObj.getToolPrices()
+        for price in prices:
+            log.info(price.text)
+            assert '$' in price.text, 'Wrong currency format mentioned for the tools.'
             
 
     
