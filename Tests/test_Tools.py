00b509b0c955ae22e0ab9414f4b5aa4e9da1f232
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2025-01-06 00:40:56 +0530

    Wrote another test case, test_ToolDetails

--- Tests/test_Tools.py
+++ Tests/test_Tools.py
@@ -8,7 +8,7 @@ class TestingTools(BaseClass):
     def test_TotalNumberOfTools(self):
         log = self.getLogger()
         homeObj = HomePage(self.driver)
-        self.verifyPageLoad(homeObj.getToolContainerElement()[1])
+        self.verifyPageLoad(homeObj.getToolContainerElement())
         totalTools = homeObj.getToolList()
         log.info("Total number of tools: {}".format(len(totalTools)))
         assert len(totalTools) == 26, 'The total number of tools should be 26.'
@@ -16,7 +16,7 @@ class TestingTools(BaseClass):
     def test_Capitalization(self):
         log = self.getLogger()
         homeObj = HomePage(self.driver)
-        self.verifyPageLoad(homeObj.getToolContainerElement()[1])
+        self.verifyPageLoad(homeObj.getToolContainerElement())
         names = homeObj.getToolNames()
         for tool in names:
             toolNameSplit = (tool.text).split(' ')
@@ -29,12 +29,24 @@ class TestingTools(BaseClass):
     def test_PriceFormat(self):
         log = self.getLogger()
         homeObj = HomePage(self.driver)
-        self.verifyPageLoad(homeObj.getToolContainerElement()[1])
+        self.verifyPageLoad(homeObj.getToolContainerElement())
         prices = homeObj.getToolPrices()
+        priceList = []
         for price in prices:
-            log.info(price.text)
+            priceList.append(price.text)
             assert '$' in price.text, 'Wrong currency format mentioned for the tools.'
+        log.info(priceList)
             
+    def test_ToolDetails(self):
+        log = self.getLogger()
+        homeObj = HomePage(self.driver)
+        self.verifyPageLoad(homeObj.getToolContainerElement())
+        for i in range(1, 27):
+            toolDetails = homeObj.getIndividualTool(str(i))
+            log.info(toolDetails[0].getProductName() + ' ' + toolDetails[0].getProductPrice()) 
+            assert toolDetails[0].getProductName() == toolDetails[1] and toolDetails[0].getProductPrice() in toolDetails[2], 'The name of the Tool or the Price of the tool is incorrect'
+            self.driver.back()
+            self.verifyPageLoad(homeObj.getToolContainerElement())
 
     
 
