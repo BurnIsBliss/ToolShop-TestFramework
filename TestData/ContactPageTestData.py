8e4c025d794b01989c6f534530b82c085ff110a4
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2025-01-08 23:25:13 +0530

    Added tests for Dropdown and Message fields, made changes within the TestData folder for these tests.

--- TestData/ContactPageTestData.py
+++ TestData/ContactPageTestData.py
@@ -1,3 +1,5 @@
 class ContactPageData:
     nameData = [('first', 'last'), ('last', 'first'), ('Good', 'Bad'), ('ßyte', 'şecular')]
     emailData = ['a@a.aa', 'b@192.168.1.68', 'Fiona_Cavanagh6324@y96lx.edu', 'George_Wilton8913@yahoo.store', 'test.io.com', 'test@io@epam.com', 'test-io@epam-usa.com', 'test(io"epam)example]com']
+    dropdownData = range(1, 7)
+    messageData = ['abcefghijklmnopqrstuvwxyz', 'abcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyz', 'abcefghijklmnopqrstuvwxyzßabcefghijklmnopqrstuvwxyzð', 'abcefghijklmnopqrstuvwxyz12341234234abcefghijklmnopqrstuvwxyz===-22', 'abcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyzabcefghijklmnopqrstuvwxyz']
--- Tests/test_contactPage.py
+++ Tests/test_contactPage.py
@@ -1,4 +1,4 @@
-import pytest, time
+import pytest
 
 from Utilities.BaseClass import BaseClass
 from PageObjects.HomePage import HomePage
@@ -33,6 +33,35 @@ class TestContactPage(BaseClass):
         contactPageObj.clickSubmit()
         successMessage = contactPageObj.getSuccessMessageElement()
         log.info('Email ID: {}'.format(email))
-        assert len(successMessage) > 0, 'The test with for the value: {}'.format(email)
+        assert len(successMessage) > 0, 'The test with the value: {} FAILED'.format(email)
         self.driver.refresh()
 
+    @pytest.mark.parametrize('index', ContactPageData.dropdownData)
+    def test_Dropdown(self, index):
+        log = self.getLogger()
+        homePageObj = HomePage(self.driver)
+        contactPageObj = homePageObj.navigateContactPage()
+        contactPageObj.populateNameElements('First Name', 'Last Name')
+        contactPageObj.populateEmailElement('email@m.mm')
+        contactPageObj.selectDropDownValue(index)
+        contactPageObj.populateMessage('abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz')
+        contactPageObj.clickSubmit()
+        successMessage = contactPageObj.getSuccessMessageElement()
+        log.info('Index Value: {}'.format(index))
+        assert len(successMessage) > 0, 'The test with the value: {} FAILED!!!'.format(index)
+        self.driver.refresh()
+    
+    @pytest.mark.parametrize('message', ContactPageData.messageData)
+    def test_MessageField(self, message):
+        log = self.getLogger()
+        homePageObj = HomePage(self.driver)
+        contactPageObj = homePageObj.navigateContactPage()
+        contactPageObj.populateNameElements('First Name', 'Last Name')
+        contactPageObj.populateEmailElement('email@m.mm')
+        contactPageObj.selectDropDownValue(1)
+        contactPageObj.populateMessage(message)
+        contactPageObj.clickSubmit()
+        successMessage = contactPageObj.getSuccessMessageElement()
+        log.info('Message: {}'.format(message))
+        assert len(successMessage) > 0, 'The test with the value: {} FAILED!!!'.format(message)
+        self.driver.refresh()
